98a00ec223cf63eba5697a9d571884e639f75fd6
Amy Dalziel <amy.dalziel@keyin.com>
2024-02-19 23:14:13 -0330

    Code written for all config options

---
+++ templates.js
@@ -0,0 +1,13 @@
+const configJson = {
+    "name": "Semester3CLI",
+    "version": "1.0.0",
+    "description": "The Command Line Interface (CLI) for the Semester 3 MidTerm Sprint.",
+    "main": "index.js",
+    "superuser": "admin01",
+    "database": "exampleDatabase"
+  }
+
+
+  module.exports = {
+    configJson,
+  };
